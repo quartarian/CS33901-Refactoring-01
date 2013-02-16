fd74b5947161186caae08f0ac3d43b604376e8e4
Garrett Bates <garrett.bates@ymail.com>
2013-02-16 04:48:17 -0500

    Update Rental.cpp

--- Rental.cpp
+++ Rental.cpp
@@ -1,4 +1,3 @@
-
 /*
   Rental.cpp
 
@@ -28,32 +27,11 @@ const Movie& Rental::getMovie() const {
 }
 
 double Rental::getCharge() const {
-
-      double thisAmount = 0;
-      switch(getMovie().getPriceCode()) {
-
-        case Movie::REGULAR:
-                thisAmount += 2;
-                if (getDaysRented() > 2)
-                    thisAmount += (getDaysRented() - 2) * 1.5;
-                break;
-
-        case Movie::NEW_RELEASE:
-                thisAmount += getDaysRented() * 3;
-                break;
-
-        case Movie::CHILDRENS:
-                thisAmount += 1.5;
-                if (getDaysRented() > 3)
-                    thisAmount += (getDaysRented() - 3) * 1.5;
-                break;
-      }
-
-  return thisAmount;
+	return movie.getCharge(days);
 }
 
 int Rental::getFrequentRenterPoints(int& frequentRenterPoints, std::vector<Rental>::const_iterator& it) const {
-  // every rental is a rental point
+	// every rental is a rental point
 	++frequentRenterPoints;
 	// new releases rented for more then one day gives a bonus rental point
 	if (it->getMovie().getPriceCode() == Movie::NEW_RELEASE && it->getDaysRented() > 1)
